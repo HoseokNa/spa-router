ab9f255a708b6e368cae3c424dbd3311bd936b14
Hoseok Na <hsna7024@gmail.com>
2022-10-19 22:35:06 +0900

    rename: Root, About pages 폴더로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import About from './components/About'
-import Root from './components/Root'
+import About from './pages/About'
+import Root from './pages/Root'
 import Route from './components/Route'
 import Router from './components/Router'
 
